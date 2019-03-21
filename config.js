4e8fe41f68b2dde17e446ee9b736dadb82e7186f
blake <blakehause@Blakes-MBP.fc.ca>
2019-03-21 16:45:38 -0400

    stylized and working routes

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: "root",
+    password: "root",
+    port: 8889, // for windows 3306
+    host: "localhost",
+    database: "marvelcharacters"
+}
+
+module.exports = config;  // too make it public
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
-var router = express.Router();
+var connect = require('../utils/sqlConnect');
+var router = express.Router(); //router coming in 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // get data from the db, and then sent it through the route
+  // to the index.hbs page, and render that (including our DB data)
+
+  // make a query from the database, if query is not successful run this error.
+  connect.query(`SELECT name, avatar FROM hero`, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
+      res.render('index', { avatars: result }); // this gets sent to layout.hbs
+    }
+  });
+  //res.render('index', { title: 'Express' });
+});
+
+// get one hero's bio information
+
+router.get('/:hero', function(req, res, next) {
+  // get data from the db, and then sent it through the route
+  // to the index.hbs page, and render that (including our DB data)
+
+  // make a query from the database, if query is not successful run this error.
+  connect.query(`SELECT * FROM hero WHERE name="${req.params.hero}"`, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
+     res.render('bio', { bioData: result[0] }); // this gets sent to layout.hbs
+    }
+  });
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
